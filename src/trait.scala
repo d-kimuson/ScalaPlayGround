f4f3853a89ec50f38ac3f5562e654bc5e96d211a
Kaito <mail.kaito03@gmail.com>
2019-12-21 03:12:10 +0900

    fix

--- src/trait.scala
+++ src/trait.scala
@@ -1,13 +1,15 @@
 object PlayGround {
   def main(args: Array[String]): Unit = {
-    val e1 = new Employee(name="Taro")
+    val e1 = new Employee(name = "Taro")
     e1.display()
   }
 }
+
 
 class Employee(val name: String) extends AnyRef with Namable
 
 trait Namable {
   val name: String
+
   def display(): Unit = println(name)
 }
